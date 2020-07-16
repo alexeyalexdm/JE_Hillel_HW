85e8c72a15b4bf6c8faec40ad04a7b71b07a7caa
Alexey <alexeyalexdm@gmail.com>
2020-07-16 16:54:42 +0300

    added homework

---
+++ HW1/Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+    }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+}
+    }
+    }
